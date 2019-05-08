4aaabe5d19c84e494b3255784084fabedb83c202
Jeron Aldaron Lau <jeronaldaron@gmail.com>
2019-05-08 00:03:33 -0500

    Work on server.

--- README.md
+++ README.md
@@ -2,8 +2,23 @@
 Ogg streaming broadcast &amp; listen application.
 
 # Setting Up
+First, we'll set up the network so we can run a web server:
+
+```bash
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 8080
+```
+
 Install dependencies.
 
 `cargo install systemfd cargo-watch --force`
 
+## Install
+```
+./install.sh
+```
 
+## Run Koggie
+```
+sudo $HOME/.cargo/bin/koggie 10.0.0.159
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{server, App, HttpRequest};
+use actix_web::{server, App as ActixApp, HttpRequest, Responder};
 use listenfd::ListenFd;
 
 fn index(_req: &HttpRequest) -> impl Responder {
@@ -6,16 +6,17 @@ fn index(_req: &HttpRequest) -> impl Responder {
 }
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
     let mut listenfd = ListenFd::from_env();
     let mut server = server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(index))
+        ActixApp::new().resource("/", |r| r.f(index))
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)
     } else {
-        server.bind("127.0.0.1:3000").unwrap()
+        server.bind(format!("{}:80", args[1])).unwrap()
     };
 
     server.run();
